79c687dc906383a90a9b68e0e1fcdc150004a184
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-11-18 14:57:15 -0300

    Meu primeiro commit

---
+++ src/java/com/CONTROLE-ESTOQUE/app/Material.java
@@ -0,0 +1,5 @@
+package java.com.CONTROLEESTOQUE.app;
+
+public class Material {
+    
+}
---
+++ src/test/java/estoque/estoquecec/EstoquececApplicationTests.java
@@ -0,0 +1,13 @@
+package estoque.estoquecec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoquececApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
