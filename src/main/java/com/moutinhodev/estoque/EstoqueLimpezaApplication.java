0a85317be6af7f7161c1187da50c99db97a6d9ad
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-11-18 15:16:07 -0300

    alteração de dependencias

---
+++ src/main/java/com/moutinhodev/estoque/EstoqueLimpezaApplication.java
@@ -0,0 +1,13 @@
+package com.moutinhodev.estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueLimpezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueLimpezaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moutinhodev/estoque/EstoqueLimpezaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moutinhodev.estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueLimpezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
